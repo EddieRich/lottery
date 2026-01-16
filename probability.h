e45547d2503e75ca5d4850ece445e9d55619f7e4
EddieRich <ehandrich@gmail.com>
2026-01-16 15:41:38 -0500

    not using probability routines, removed

--- probability.h
+++
@@ -1,7 +0,0 @@
-#ifndef __PROBABILITY_DEFINED__
-#define __PROBABILITY_DEFINED__
-
-long double nCm(int, int);
-long double calculate_odds(int N1, int D1, int M1, int N2, int D2, int M2);
-
-#endif // __PROBABILITY_DEFINED__
